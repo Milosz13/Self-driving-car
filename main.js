1fc3119c2077e90474ed312b30f4d3e2e418a811
Milosz <milosz.zalinski@gmail.com>
2022-06-08 19:34:08 +0200

    added driving car

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
-const canvas = document.getElementById("canva");
-canvas.width =200;
+const canvas=document.getElementById("myCanvas");
+canvas.width=200;
 
-const ctx = canvas.getContext('2d');
-const road = new Road(canvas.width/2, canvas.width*0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
-car.draw(ctx);
+const ctx = canvas.getContext("2d");
+const road=new Road(canvas.width/2,canvas.width*0.9);
+const car=new Car(road.getLaneCenter(1),100,30,50, "KEYS");
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50, "IMITATION", 1.5)
+];
 
 animate();
 
 function animate(){
-    car.update();
-    canvas.height = innerHeight;
-    ctx.save();
-    ctx.translate(0, -car.y+canvas.height*0.7);
+    for(let i = 0; i<traffic.length; i++){
+        traffic[i].update(road.borders, []);
+    }
+    car.update(road.borders, traffic);
 
-    road.draw(ctx); 
+    canvas.height=window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
+
+    road.draw(ctx);
+    for(let i = 0; i<traffic.length; i++){
+        traffic[i].draw(ctx);
+    }
     car.draw(ctx);
 
     ctx.restore();

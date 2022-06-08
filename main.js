ba62f8538b698657def676203e032a92cfa9d0af
Milosz <milosz.zalinski@gmail.com>
2022-06-08 22:46:22 +0200

    network changes

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-const carCanvas=document.getElementById("carCanvas");
-carCanvas.width=200;
+const Canva=document.getElementById("Canva");
+Canva.width=200;
 
-const networkCanvas=document.getElementById("networkCanvas");
-networkCanvas.width=300;
 
-const carCtx = carCanvas.getContext("2d");
-const networkCtx = carCanvas.getContext("2d");
+const ctx = Canva.getContext("2d");
 
-const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
+const road=new Road(Canva.width/2,Canva.width*0.9);
 const car=new Car(road.getLaneCenter(1),100,30,50, "AI");
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "IMITATION", 1.5)
@@ -21,19 +18,18 @@ function animate(){
     }
     car.update(road.borders, traffic);
 
-    carCanvas.height=window.innerHeight;
-    networkCanvas.height=window.innerHeight;
+    Canva.height=window.innerHeight;
 
 
-    carCtx.save();
-    carCtx.translate(0,-car.y+carCanvas.height*0.7);
+    ctx.save();
+    ctx.translate(0,-car.y+Canva.height*0.7);
 
-    road.draw(carCtx);
+    road.draw(ctx);
     for(let i = 0; i<traffic.length; i++){
-        traffic[i].draw(carCtx);
+        traffic[i].draw(ctx);
     }
-    car.draw(carCtx);
+    car.draw(ctx);
 
-    carCtx.restore();
+    ctx.restore();
     requestAnimationFrame(animate);
 }

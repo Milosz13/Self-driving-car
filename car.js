fd4d68913b7916bfe74788353cd1748a8a618893
Milosz <milosz.zalinski@gmail.com>
2022-06-08 22:37:26 +0200

    added neural network

--- car.js
+++ car.js
@@ -11,8 +11,13 @@ class Car{
         this.friction=0.05;
         this.angle=0;
 
+        this.useBrain = controlType=="AI";
+
         if(controlType !="IMITATION"){
             this.sensor=new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
         }
         this.controls=new Controls(controlType);
     }
@@ -24,7 +29,19 @@ class Car{
             this.damaged = this.#assesDamage(roadBorders, traffic);
         }
         if(this.sensor){
-            this.sensor.update(roadBorders, traffic);       
+            this.sensor.update(roadBorders, traffic);  
+            const offsets = this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+
+            if(this.useBrain){
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
